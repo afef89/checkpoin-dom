f7ccfe12c911e35a8fe86e130b324dbbf0f5e158
afef89 <mabrouk_afef@yahoo.fr>
2021-11-04 10:01:14 +0100

    dom project

---
+++ index.js
@@ -0,0 +1,110 @@
+addToCartBtn = document.getElementsByClassName('addToCartBtn');
+cartContainer = document.getElementsByClassName('cartContainer')[0];
+var plusBtn = document.getElementById("plus-btn");
+var minBtn = document.getElementById("min-btn");
+var qty = +document.getElementById("quantity").innerHTML;
+var qtya = +document.getElementById("quantity2").innerHTML;
+
+plusBtn.addEventListener("click", function () {
+  document.getElementById("quantity").innerHTML = qty++;
+});
+
+minBtn.addEventListener("click", function () {
+  if (qty > 0) {
+    document.getElementById("quantity").innerHTML = qty--;
+  } else {
+    alert("pay attention");
+    
+  }
+});
+plusBtn.addEventListener("click", function () {
+    document.getElementById("quantity2").innerHTML = qtya++;
+  });
+  
+  minBtn.addEventListener("click", function () {
+    if (qtya > 0) {
+      document.getElementById("quantity2").innerHTML = qtya--;
+    } else {
+      alert("pay attention");
+      
+    }
+  });
+
+
+for (var i = 0; i < addToCartBtn.length; i++) {
+    addToCartBtn[i].addEventListener('click', updateCartContainer)
+}
+
+function updateCartContainer(e) {
+    addToCartBtn = e.target;
+    productContainer = addToCartBtn.parentElement;
+    productTitle = productContainer.getElementsByClassName('productTitle')[0].innerText;
+    productPrice = productContainer.getElementsByClassName('productPrice')[0].innerText;
+    productimg = productContainer.getElementsByClassName('img')[0].src;
+
+    cartTitles = cartContainer.getElementsByClassName('cartTitle');
+    for (var i = 0; i < cartTitles.length; i++) {
+        if (cartTitles[i].innerText == productTitle) {
+            alert('Product already to cart');
+            return;
+        }
+    }
+
+
+    AddRowInCart(productTitle, productPrice, productimg);
+
+
+
+
+
+}
+
+// add new row in cart
+function AddRowInCart(productTitle, productPrice, productimg) {
+
+
+    div = document.createElement('div');
+    div.classList.add('row');
+    insideDiv = `<div class="col-xs-3 cartImage"> <img src="${productimg}"></div>
+<div class="col-xs-3 cartTitle"> ${productTitle} </div>
+<div class="col-xs-3 cartPrice"> ${productPrice}</div>
+ <div class="col-xs-3 removeButton"> Remove</div>`;
+    div.innerHTML = insideDiv
+    cartContainer.appendChild(div);
+    totalPrice = 0;
+    cartPrice = cartContainer.getElementsByClassName('cartPrice');
+    for (var i = 0; i < cartPrice.length; i++) {
+        priceFormatedNumb = cartPrice[i].innerText.replace('₹', '').replace(',', '');
+        totalPrice = totalPrice + parseInt((priceFormatedNumb));
+    }
+    document.getElementsByClassName('cartTotalPrice')[0].innerText = new Number(totalPrice).toLocaleString('en');;
+
+
+
+    removeButton = document.getElementsByClassName('removeButton');
+    for (var i = 0; i < removeButton.length; i++) {
+        removeButton[i].addEventListener('click', removeFromCart)
+    }
+}
+
+
+
+// remove product from Cart
+
+function removeFromCart(e) {
+    e.target.parentElement.remove();
+    updatePriceAfterRemove();
+}
+
+
+function updatePriceAfterRemove() {
+    totalPrice = 0;
+    cartPrice = cartContainer.getElementsByClassName('cartPrice');
+    for (var i = 0; i < cartPrice.length; i++) {
+        priceFormatedNumb = cartPrice[i].innerText.replace('₹', '').replace(',', '');
+        totalPrice = totalPrice + parseInt((priceFormatedNumb));
+    }
+    document.getElementsByClassName('cartTotalPrice')[0].innerText = new Number(totalPrice).toLocaleString('en');
+
+
+}
